b58a2bd295f4f91e1e842e5dd143c5274b57f0b2
David Turnbull <realdavidturnbull@gmail.com>
2018-05-02 16:11:17 +1000

    Create “UpdateFirmware” class

--- app.rb
+++ app.rb
@@ -1,4 +1,38 @@
-require "./device_list"
+require "./lib/ipsw/device_list"
+require "./lib/ipsw/identifier"
+
+# class UpdateFirmware
+
+#     IPSW_DIRECTORY = ""
+
+#     attr_accessor :devices, :urls
+
+#     def initialize
+#         @devices = IPSW::DeviceList.new(only: "iPhone")
+#         @urls = Set.new
+#     end
+
+#     def update!
+#         devices.identifiers.each do |identifier|
+#             firmware = IPSW::Identifier.new(identifier)
+#             urls << firmware.latest[:url]
+#             firmware.previous.each do |firmware|
+#                     if File.exists?(firmware[:file_name])
+#                         puts "Deleting #{firmware[:file_name]}..."
+#                         File.delete(firmware[:file_name])
+#                     end
+#                 end
+#             end
+#         end
+#     end
+
+#     def write_to_file
+#         File.open("urls.txt", "w") do |file|
+#             file.write(urls.to_a.join("\n"))
+#         end
+#     end
+
+# end
 
 IPSW_DIRECTORY = ""
 
