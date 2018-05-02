58b05dcf1baa92c627d4008c728546b02687ed55
David Turnbull <realdavidturnbull@gmail.com>
2018-05-02 16:10:28 +1000

    Update cron schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,15 +1,21 @@
+require "ptools"
+
 every 5.minutes do
     # Update urls in text file
-    # firmware = FetchLatestFirmware.new
+    # firmware = UpdateFirmware.new
     # firmware.update!
 end
 
 # Download files from text file
 every 10.minutes do
-    command `aria2
-                --continue
-                --allow-overwrite=false
-                --always-resume=true
-                --conditional-get=true
-                --input urls.txt`
+    if File.which("aria2c")
+        command `aria2c
+                    --continue
+                    --allow-overwrite=false
+                    --always-resume=true
+                    --conditional-get=true
+                    --input urls.txt`
+    else
+        raise "aria2 is required to download files. Download here: https://aria2.github.io"
+    end
 end
