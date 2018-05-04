63082d69a8f8a24d2309892d6331ec9b980524ad
David Turnbull <realdavidturnbull@gmail.com>
2018-05-04 17:59:20 +1000

    Update schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -9,12 +9,12 @@ end
 # Download files from text file
 every 10.minutes do
 #     if File.which("aria2c")
-        command `aria2c
+        command "aria2c
                     --continue
                     --allow-overwrite=false
                     --always-resume=true
                     --conditional-get=true
-                    --input urls.txt`
+                    --input urls.txt"
 #     else
 #         raise "aria2 is required to download files. Download here: https://aria2.github.io"
 #     end
