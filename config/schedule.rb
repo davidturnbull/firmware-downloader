6ad3bfad3960706a107bc8de8345d46aaa9c9838
David Turnbull <realdavidturnbull@gmail.com>
2018-05-04 17:58:29 +1000

    Update schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,4 @@
-require "ptools"
+# require "ptools"
 
 every 5.minutes do
     # Update urls in text file
@@ -8,14 +8,14 @@ end
 
 # Download files from text file
 every 10.minutes do
-    if File.which("aria2c")
+#     if File.which("aria2c")
         command `aria2c
                     --continue
                     --allow-overwrite=false
                     --always-resume=true
                     --conditional-get=true
                     --input urls.txt`
-    else
-        raise "aria2 is required to download files. Download here: https://aria2.github.io"
-    end
+#     else
+#         raise "aria2 is required to download files. Download here: https://aria2.github.io"
+#     end
 end
