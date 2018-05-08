56ae1524ee7eb14aebb3c921fe6b1fc394705587
David Turnbull <realdavidturnbull@gmail.com>
2018-05-08 20:15:35 +1000

    Simplify Whenever schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,19 +1,10 @@
-# require "ptools"
-
+# Update URLS
 every 5.minutes do
     rake "update"
 end
 
 # Download files from text file
 every 10.minutes do
-#     if File.which("aria2c")
-        command "aria2c \
-                    --continue \
-                    --allow-overwrite=false \
-                    --always-resume=true \
-                    --input=/root/firmware-downloader/urls.txt \
-                    --dir=/mnt/volume-sfo2-01"
-#     else
-#         raise "aria2 is required to download files. Download here: https://aria2.github.io"
-#     end
+    # TODO: Make directories if they don't exist
+    command "aria2c --continue --allow-overwrite=false --always-resume=true --input=/root/app/urls.txt --dir=/root/files"
 end
